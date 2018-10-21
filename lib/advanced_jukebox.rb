edcf6361977ab0e7c01ed510ffe7fc02b23d5e09
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-21 17:56:41 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -44,7 +44,7 @@ def play(my_songs)
   if my_songs.keys.include?(response)
     puts "Playing #{response}"
     #binding.pry
-    system "start wmplayer #{my_songs[response]}"
+    start wmplayer my_songs[response]
   else
     puts "Invalid input, please try again"
   end
